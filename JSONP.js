7dc23f1182be5191b6b607597a71b2c933c88e23
coolFish <2596915059@qq.com>
2021-05-17 23:34:57 +0800

    feat: jsonp

---
+++ JSONP.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+app.get("/index", (req, res) => {
+  let { params, callback } = req.query;
+  console.log(params); // coolFish
+  console.log(callback); // callback
+  res.send(`${callback}('我是郭老师')`);
+}),
+  app.listen(3000, () => {
+    console.log("服务开启成功"); //yellow
+  });
